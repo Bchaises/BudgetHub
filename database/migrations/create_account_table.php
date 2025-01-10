3174993bbfe22af0dc3f2c7701b3eac834cf272e
Bchaises <benjamin.chaises@gmail.com>
2025-01-10 17:32:09 +0100

    add migration for account, transaction and transaction_category table

---
+++ database/migrations/create_account_table.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('account', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('balance', 65, 30)->default(0.00);
+            $table->string('description')->nullable();
+            $table->text('title');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('account');
+    }
+};
