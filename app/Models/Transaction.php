3fbe74265790c370959e433ca45185f24b8aeff6
Bchaises <benjamin.chaises@gmail.com>
2025-01-13 18:16:01 +0100

    Adding Account, Transaction and TransactionCategory Models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    protected $fillable = [
+        'label',
+        'amount',
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(TransactionCategory::class);
+    }
+}
