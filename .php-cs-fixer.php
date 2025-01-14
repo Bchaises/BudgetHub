7b115071798853b90a78d1a06fa9c32775a60b8d
Bchaises <benjamin.chaises@gmail.com>
2025-01-14 16:33:17 +0100

    Updating php-cs-fixer config.

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,13 +1,39 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__)
-;
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
 
-return (new PhpCsFixer\Config())
+$finder = Finder::create()
+    ->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+        __DIR__ . '/resources',
+        __DIR__ . '/tests',
+    ])
+    ->exclude([
+        'storage',
+        'vendor',
+        'node_modules',
+        'bootstrap/cache',
+    ])
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+return (new Config())
     ->setRules([
-        '@PER-CS' => true,
-        '@PHP82Migration' => true,
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'binary_operator_spaces' => ['default' => 'single_space'],
+        'no_unused_imports' => true,
+        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'single_quote' => true,
+        'class_definition' => [
+            'space_before_parenthesis' => false,
+        ],
     ])
     ->setFinder($finder)
-;
+    ->setUsingCache(true);
