273d8c2363fca2cf2b8c3e40704a2a77160b7d44
Bchaises <benjamin.chaises@gmail.com>
2025-01-14 16:20:43 +0100

    Adding show and update functions to AccountController.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -3,23 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Models\Account;
+use App\Models\Transaction;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class AccountController extends Controller
 {
-    public function show(): View
+    public function index(): View
     {
-        return view('account', [
+        return view('account.index', [
             'title' => 'Account',
             'accounts' => Account::all(),
+        ]);
+    }
+
+    public function show(string $id): View
+    {
+        return view('account.show', [
+            'title' => 'Account',
+            'account' => Account::find($id),
+            'transactions' => Transaction::where('account_id', $id)->get(),
         ]);
     }
 
     public function store(Request $request): RedirectResponse
     {
         $account = new Account();
+        $account->title = $request->title;
+        $account->description = $request->description;
+        $account->balance = $request->balance;
+        $account->save();
+
+        return redirect('/account');
+    }
+
+    public function update(Request $request, string $id): RedirectResponse
+    {
+        $account = Account::find($id);
         $account->title = $request->title;
         $account->description = $request->description;
         $account->balance = $request->balance;
