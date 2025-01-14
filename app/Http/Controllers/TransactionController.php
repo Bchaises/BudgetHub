d058ba81ded991083423d0cb424e73a53fbb5df8
Bchaises <benjamin.chaises@gmail.com>
2025-01-14 16:20:22 +0100

    Adding store function to TransactionController.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,17 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use App\Models\Transaction;
+use App\Models\TransactionCategory;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class TransactionController extends Controller
 {
-    public function show(): View
+    public function index(): View
     {
-        return view('transaction', [
+        return view('transaction.index', [
             'title' => 'Transaction',
             'transactions' => Transaction::all(),
+            'accounts' => Account::all(),
+            'categories' => TransactionCategory::all(),
         ]);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $transaction = new Transaction();
+        $transaction->label = $request->label;
+        $transaction->amount = $request->amount;
+        $transaction->status = $request->status;
+        $transaction->account_id = $request->account;
+        $transaction->category_id = $request->category;
+        $transaction->save();
+
+        return redirect('/transaction');
     }
 }
