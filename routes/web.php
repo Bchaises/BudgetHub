ca8f250845ea8dd258c62d5b07f4ed0daf67e250
Bchaises <benjamin.chaises@gmail.com>
2025-01-15 15:45:21 +0100

    Updating routes with unique names.

--- routes/web.php
+++ routes/web.php
@@ -6,22 +6,22 @@ use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 // Dashboard
-Route::get('/', [DashboardController::class, 'show']);
+Route::get('/', [DashboardController::class, 'show'])->name('dashboard');
 
 // Routes pour les transactions
 Route::prefix('transaction')->controller(TransactionController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/{id}', 'show')->name('show');
-    Route::post('/store', 'store')->name('store');
-    Route::patch('/update/{id}', 'update')->name('update');
-    Route::delete('/delete/{id}', 'delete')->name('delete');
+    Route::get('/', 'index')->name('transaction.index');
+    Route::get('/{id}', 'show')->name('transaction.show');
+    Route::post('/store', 'store')->name('transaction.store');
+    Route::patch('/update/{id}', 'update')->name('transaction.update');
+    Route::delete('/delete/{id}', 'delete')->name('transaction.delete');
 });
 
 // Routes pour les comptes
 Route::prefix('account')->controller(AccountController::class)->group(function () {
-    Route::get('/', 'index')->name('index');
-    Route::get('/{id}', 'show')->name('show');
-    Route::post('/store', 'store')->name('store');
-    Route::patch('/update/{id}', 'update')->name('update');
-    Route::delete('/delete/{id}', 'delete')->name('delete');
+    Route::get('/', 'index')->name('account.index');
+    Route::get('/{id}', 'show')->name('account.show');
+    Route::post('/store', 'store')->name('account.store');
+    Route::patch('/update/{id}', 'update')->name('account.update');
+    Route::delete('/delete/{id}', 'delete')->name('account.delete');
 });
