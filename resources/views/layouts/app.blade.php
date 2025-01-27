d2a88e3ad324c95f581334507a5ef57070873467
Bchaises <benjamin.chaises@gmail.com>
2025-01-27 17:59:37 +0100

    Updating Navigation Bar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,20 +16,12 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-default antialiased">
-        <div class="min-h-screen bg-gray-100">
+        <div class="h-screen w-screen flex bg-gray-100">
+            <!-- Navigation Bar -->
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
             <!-- Page Content -->
-            <main>
+            <main class="mx-32 h-screen w-screen">
                 {{ $slot }}
             </main>
         </div>
